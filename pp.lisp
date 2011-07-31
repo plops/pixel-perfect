76b97ade722cb3c0de7f45c20d0a7214c7cfba43
martin <martin@hop.kcl.ac.uk>
2011-07-31 18:52:19 +0100

    draw several phases in colors, draw lines of width 1 next to each other to increase line-width

--- pp.lisp
+++ pp.lisp
@@ -19,28 +19,30 @@
 ;; x=const=q
 ;; y=sqrt [R^2- (q-cx)^2]+cy
 
-(defun draw-grating-disk (center-x center-y radius phase width)
-  (loop for xx from (round (- center-x radius))
-     upto (round (+ center-x radius)) by width do
-       (let* ((x (+ xx phase))
-	      (q (- (expt radius 2)
-		    (expt (- center-x x) 2))))
-	 (when (< 0 q)
-	   (let ((y (sqrt q)))
-	     (vertex x (+ center-y y))
-	     (vertex x (- center-y y)))))))
+(defun draw-grating-disk (center-x center-y radius &key 
+			  (phase 0) (phases 3) (line-width 4))
+  (with-primitive :lines
+    (let ((pl (* phases line-width)))
+     (loop for xx from (round (- center-x radius))
+	upto (round (+ center-x radius)) by pl do
+	(let ((xxx (+ xx (* phase line-width))))
+	  (loop for x from xxx below (+ xxx line-width) do
+	       (let* ((q (- (expt radius 2)
+			    (expt (- center-x x) 2))))
+		 (when (< 0 q)
+		   (let ((y (sqrt q)))
+		     (vertex x (round (+ center-y y)))
+		     (vertex x (round (- center-y y))))))))))))
 
 (defparameter *phase* 0)
 (let ((bla nil))
  (defun draw-screen ()
    (clear :color-buffer-bit)
-   (line-width 1)
-   (let ((width 3))
-    (with-primitive :lines
-      (incf *phase*)
-      (when (< width *phase*)
-	(setf *phase* 0))
-      (draw-grating-disk 150 150 100 *phase* width)))))
+   (let ((phases 3))
+     (loop for (p c) in '((0 (.8 .3 0)) (1 (.3 .6 .2)) (2 (.3 .3 .9))) do
+      (apply #'color c)
+      (draw-grating-disk 150 150 100 :phase p
+			 :phases phases :line-width 4)))))
 
 #+nil
 (let ((x 790)
